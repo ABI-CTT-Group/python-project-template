23520986b8603f2fe9e27e6481f37bd35bdfc50b
Chinchien Lin <lin810116@gmail.com>
2023-11-16 14:46:10 +1300

    feat: add script header template

---
+++ package_name/script_header_template.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+"""
+{Description}
+"""
+
+# Import built-in modules. e.g.
+# import os
+# import sys
+
+# Import third-party modules. e.g.
+# import pandas as pd
+
+# Import your own modules. e.g.
+# from {path} import {class}
+
+__author__ = '{author}'
+__copyright__ = 'Copyright {year}, {project_name}'
+__credits__ = ['{credit_list}'] # people who reported bug fixes, made suggestions, etc. but did not actually write the code.
+__license__ = '{license}'
+__version__ = '{mayor}.{minor}.{rel}'
+__maintainer__ = '{maintainer}'
+__email__ = '{contact_email}'
+__status__ = '{dev_status}' # Status should typically be one of "Prototype", "Development", or "Production"
+
